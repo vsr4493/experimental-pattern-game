565067d220c9152c4f603519da17097ca2310453
Vardhman Singh <vsr4493@gmail.com>
2020-04-24 17:28:58 +0530

    fix: increase level length

--- src/PatternGame/gameConfig.js
+++ src/PatternGame/gameConfig.js
@@ -17,4 +17,4 @@ const generatePatterns = (levelCount, options = ['#c0392b', '#2980b9', '#27ae60'
   };
 }
 
-export const levelConfig = generatePatterns(2);
+export const levelConfig = generatePatterns(10);
